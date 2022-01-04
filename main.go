4fffb5581454514e48a1626a542bb7a0ac3f3120
Per von Rosen <per.von.rosen@google.com>
2022-01-04 20:00:28 +0100

    revert oidc

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"log"
-	"net/url"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	sw "github.com/pvr1/gigs/go"
-	gin_oidc "github.com/pvr1/gin-oidc"
 )
 
 func main() {
@@ -18,27 +16,8 @@ func main() {
 
 	router := sw.NewRouter()
 
-	issuer, _ := url.Parse("https://accounts.google.com")
-	clienturl, _ := url.Parse("http://localhost:8080/login")
-	postlogouturl, _ := url.Parse("http://localhost:8080/logout")
-
-	//middleware params
-	initParams := gin_oidc.InitParams{
-		Router:       router,
-		ClientId:     "xx-xxx-xxx",
-		ClientSecret: "xx-xxx-xxx",
-		Issuer:       *issuer,
-		ClientUrl:    *clienturl,
-		Scopes:       []string{"openid", "profile", "email"},
-		ErrorHandler: func(c *gin.Context) {
-			message := c.Errors.Last().Error()
-			c.String(302, message)
-		},
-		PostLogoutUrl: *postlogouturl,
-	}
-
 	//protect all endpoint below this line
-	router.Use(gin_oidc.Init(initParams), cors.New(cors.Config{
+	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"https://foo.com", "*"},
 		AllowMethods:     []string{"PUT", "PATCH"},
 		AllowHeaders:     []string{"Origin"},
